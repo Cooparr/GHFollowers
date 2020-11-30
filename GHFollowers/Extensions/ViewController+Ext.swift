8e728eff0157caf8dc50fb5f4e799407fc357619
Alex Cooper <Cooparr@hotmail.co.uk>
2020-11-30 15:02:36 +0000

    Created two function in the UIViewController extension. One for displaying a containerView with activityIndicator spinning in the centre of the view then the other for dismissing that view. Will be used for showing the user that the app is currently loading more content when they hit the bottom of the FollowersListVC CollectionView.

--- GHFollowers/Extensions/ViewController+Ext.swift
+++ GHFollowers/Extensions/ViewController+Ext.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
     
     //MARK: Present GFAlert On Main Thread
@@ -16,6 +18,38 @@ extension UIViewController {
             alertVC.modalPresentationStyle = .overFullScreen
             alertVC.modalTransitionStyle = .crossDissolve
             self.present(alertVC, animated: true)
+        }
+    }
+    
+    
+    //MARK: Show Loading View
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8 }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        containerView.addSubview(activityIndicator)
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    
+    //MARK: Dismiss Loading View
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
