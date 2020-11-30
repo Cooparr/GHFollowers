5e23e993201d4af2fa07acf725e1d14fff1d18e9
Alex Cooper <Cooparr@hotmail.co.uk>
2020-11-30 01:40:35 +0000

    Done the initial setup for the FollowersListVC collection view

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -11,14 +11,43 @@ class FollowersListVC: UIViewController {
 
     //MARK: Properties
     var username: String!
+    var collectionView: UICollectionView!
     
     
     //MARK: View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+    }
+    
+    
+    //MARK: View Will Appear
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    
+    //MARK: Collection View Controller
+    fileprivate func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-        
+    }
+    
+    
+    //MARK: Configure Collection View
+    fileprivate func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(GFFollowerCell.self, forCellWithReuseIdentifier: GFFollowerCell.reuseId)
+    }
+    
+    
+    //MARK: Get Followers
+    fileprivate func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             switch result {
             case .success(let followers):
@@ -29,12 +58,5 @@ class FollowersListVC: UIViewController {
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
         }
-    }
-    
-    
-    //MARK: View Will Appear
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 }
