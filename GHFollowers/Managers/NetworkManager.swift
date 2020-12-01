b4b942bae27d52f550839dd1cc910b623f406bbd
Alex Cooper <Cooparr@hotmail.co.uk>
2020-12-01 01:43:23 +0000

    Added a getUserInfo function to the NetworkManager which will receive a users info via a given username parameter.

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -53,4 +53,40 @@ class NetworkManager {
         }
         task.resume()
     }
+    
+    
+    //MARK: Get User
+    func getUserInfo(for username: String, completion: @escaping (Result<User, GFError>) -> Void) {
+        let endpoint = baseURL + username
+        
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.invalidData))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completion(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let user = try JSONDecoder().decode(User.self, from: data)
+                completion(.success(user))
+            } catch {
+                completion(.failure(.invalidData))
+            }
+        }
+        task.resume()
+    }
 }
