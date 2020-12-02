0532b6b862502fab2a2bb72d70a6995f18422f25
Alex Cooper <Cooparr@hotmail.co.uk>
2020-12-02 15:02:32 +0000

    Created the FavoriteCell that will be used in the FavoritesListVC to display  users favourites. Will create the table view and plug this in next.

---
+++ GHFollowers/Custom Views/Cells/GFFavoriteCell.swift
@@ -0,0 +1,56 @@
+//
+//  GFFavoriteCell.swift
+//  GHFollowers
+//
+//  Created by Alexander James Cooper on 02/12/2020.
+//
+
+import UIKit
+
+class GFFavoriteCell: UITableViewCell {
+
+    //MARK: Properties
+    static let reuseId = "FavoriteCellId"
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 26)
+    
+    
+    //MARK: Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    
+    // Set Cell Content
+    func set(favorite: Follower) {
+        usernameLabel.text = favorite.username
+        avatarImageView.downloadImage(from: favorite.avatarURL)
+    }
+    
+    
+    //MARK: Configure
+    private func configure() {
+        accessoryType = .disclosureIndicator
+        
+        let padding: CGFloat = 12
+        addSubviews(avatarImageView, usernameLabel)
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: padding * 2),
+            usernameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40),
+        ])
+    }
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
